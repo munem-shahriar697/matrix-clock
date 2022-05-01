8ef6a9ad67601dd951352bc10ca4436ae86d2b39
munem-shahriar697 <munemshahriar007@gmail.com>
2022-05-02 00:14:59 +0600

    mabe final push

---
+++ clock.js
@@ -0,0 +1,11 @@
+
+function display_c() {
+    var refresh = 1000; // Refresh rate in milli seconds
+    mytime = setTimeout('display_ct()', refresh)
+}
+
+function display_ct() {
+    var x = new Date().toLocaleTimeString('en-GB');
+    document.getElementById('ct').innerHTML = x;
+    display_c();
+}
